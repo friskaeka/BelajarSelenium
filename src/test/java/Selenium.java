2ba8a738e80c272f7de9be7c6fbd6f75be525f34
Friska Eka K <friska744@gmail.com>
2024-08-09 18:01:07 +0700

    belajar

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Selenium {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Selenium {
+
+    @Test
+    public void halloJayJay(){
+        //inisialisasi browser driver
+        WebDriver driver = WebDriverManager.chromedriver().create();
+
+        //navigasi url
+        driver.get("https://jayjay.co/");
+
+        //get element atribut
+        String text = driver.findElement(By.className("first-course")).getText();
+
+        //assersion atau pengecekan
+        assertEquals("Gebrakan Kursus Online Yang Siap Meroketkan Kariermu", text);
+
+    }
+
 }
